c1dae4bb4b63e51039e277e822efc4b4c44fda9e
Ekaterina Frolova <45566145+katyfrolova@users.noreply.github.com>
2019-02-15 18:58:15 +0300

    Add files via upload

--- to_json.py
+++ to_json.py
@@ -4,12 +4,10 @@ import functools
 def to_json(func):
     @functools.wraps(func)
     def wrapped(*args,**kwargs):
-        obj = str(func(*args,**kwargs))
+        obj = func(*args,**kwargs)
         print(json.dumps(obj))
     return wrapped
 @to_json
 def get_data():
-  return {
-      'data': 42
-  }
+    return None
 get_data()  # вернёт '{"data": 42}'
